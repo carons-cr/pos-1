f6fc82ddee643d3344a0427605b767d03bebf408
carons-cr <2738794789@qq.com>
2017-07-14 03:47:50 -0400

    add test again

--- fizzbuzzwizz/test/fizzbuzzwizz-test.js
+++ fizzbuzzwizz/test/fizzbuzzwizz-test.js
@@ -1,15 +1,72 @@
 /**
+ * Created by cr on 7/14/17.
+ */
+/**
  * Created by cr on 7/13/17.
  */
 'use strict';
 
 describe('pos', () => {
 
-    it('when number is times of 3,5,7,should return the tag', () => {
+    it('when number is times of 3,should return the tag', () => {
+    let number = 6;
+    const actualResult=getTag(number);
+
+    let expectResult = 'fizz';
+
+    expect(actualResult).toEqual(expectResult);
+    });
+
+    it('when number is times of 5,should return the tag', () => {
+        let number = 10;
+        const actualResult=getTag(number);
+
+        let expectResult = 'wizz';
+
+        expect(actualResult).toEqual(expectResult);
+    });
+
+    it('when number is times of 7,should return the tag', () => {
+        let number = 14;
+        const actualResult=getTag(number);
+
+        let expectResult = 'fizz';
+
+        expect(actualResult).toEqual(expectResult);
+    });
+
+    it('when number is times of 3,5,should return the tag', () => {
+        let number = 15;
+        const actualResult=getTag(number);
+
+        let expectResult = 'fizzbuzz';
+
+        expect(actualResult).toEqual(expectResult);
+    });
+
+    it('when number is times of 3,7,should return the tag', () => {
         let number = 21;
         const actualResult=getTag(number);
 
         let expectResult = 'fizzwizz';
+
+        expect(actualResult).toEqual(expectResult);
+    });
+
+    it('when number is times of 5,7,should return the tag', () => {
+        let number = 70;
+        const actualResult=getTag(number);
+
+        let expectResult = 'buzzwizz';
+
+        expect(actualResult).toEqual(expectResult);
+    });
+
+    it('when number is times of 3,5,7,should return the tag', () => {
+        let number = 105;
+        const actualResult=getTag(number);
+
+        let expectResult = 'fizzbuzzwizz';
 
         expect(actualResult).toEqual(expectResult);
     });
@@ -22,9 +79,9 @@ describe('pos', () => {
         let expectResult = 'fizz';
 
         expect(actualResult).toEqual(expectResult);
-    });
+        });
 
-    it('when number is times of 3,5,7 and does not contain 3,should return the tag', () => {
+    it('when number does not contain 3 and is times of 3,5,7 ,should return the tag', () => {
         let number = 15;
 
         const actualResult=fizzbuzzwizz(number);
@@ -32,9 +89,9 @@ describe('pos', () => {
         let expectResult = 'fizzbuzz';
 
         expect(actualResult).toEqual(expectResult);
-    });
+        });
 
-    it('when number is not times of 3,5,7 and does not contain 3,should return the number', () => {
+    it('when number does not contain 3 and is not times of 3,5,7,should return the number', () => {
         let number = 8;
 
         const actualResult=fizzbuzzwizz(number);
@@ -42,5 +99,6 @@ describe('pos', () => {
         let expectResult = 8;
 
         expect(actualResult).toEqual(expectResult);
-    });
+        });
+
 });
