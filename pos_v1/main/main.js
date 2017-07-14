36653b68124db958a77b18ca6868901f0881da28
carons-cr <2738794789@qq.com>
2017-07-14 06:28:29 -0400

    delete not useful code

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -98,79 +98,14 @@ function printReceipt(tags) {
         for(var j=0;j<allItems.length;j++){
             if(tags_addCost2[i].barcode==allItems[j].barcode){
                 str2+='名称：'+allItems[j].name+'，数量：'+tags_addCost2[i].count
-                    +allItems[j].unit+'，单价：'+allItems[j].price+'(元)，'
-                    +tags_addCost2[i].discount_cost+'(元)，\n';
+                    +allItems[j].unit+'，单价：'+(allItems[j].price).toFixed(2)+'(元)，小计：'
+                  +tags_addCost2[i].discount_cost+'(元)\n';
             }
         }
     }
-    var str3='----------------------\n'+'总计：'+getSum_discount_cost(tags_addCost2)+'(元)，\n'
-        +'节省：'+getSavingMoney(tags_addCost2)+'\n**********************';
+    var str3='----------------------\n'+'总计：'+getSum_discount_cost(tags_addCost2)+'(元)\n'
+        +'节省：'+getSavingMoney(tags_addCost2)+'(元)'+'\n**********************';
     result=str1+str2+str3;
     //return result;
     console.log(result);
 }
-var tags = [
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000003-2.5',
-    'ITEM000005',
-    'ITEM000005-2',
-];
-function loadAllItems() {
-    return [
-        {
-            barcode: 'ITEM000000',
-            name: '可口可乐',
-            unit: '瓶',
-            price: 3.00
-        },
-        {
-            barcode: 'ITEM000001',
-            name: '雪碧',
-            unit: '瓶',
-            price: 3.00
-        },
-        {
-            barcode: 'ITEM000002',
-            name: '苹果',
-            unit: '斤',
-            price: 5.50
-        },
-        {
-            barcode: 'ITEM000003',
-            name: '荔枝',
-            unit: '斤',
-            price: 15.00
-        },
-        {
-            barcode: 'ITEM000004',
-            name: '电池',
-            unit: '个',
-            price: 2.00
-        },
-        {
-            barcode: 'ITEM000005',
-            name: '方便面',
-            unit: '袋',
-            price: 4.50
-        }
-    ];
-}
-
-function loadPromotions() {
-    return [
-        {
-            type: 'BUY_TWO_GET_ONE_FREE',
-            barcodes: [
-                'ITEM000000',
-                'ITEM000001',
-                'ITEM000005'
-            ]
-        }
-    ];
-}
-
-printReceipt(tags);
