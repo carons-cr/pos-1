0b94beb9529d565282d8c1afedcf117a222df63b
carons-cr <2738794789@qq.com>
2017-07-14 06:29:54 -0400

    add test

--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -2,7 +2,78 @@
 
 describe('pos', () => {
 
-  it('should print text', () => {
+    it('should return ags_addCount in there the barcode of tags is the only one and contain count', () => {
+        const tags = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+
+        const actualResult=getTags_addCount(tags);
+
+        let expectResult = [
+            {barcode:'ITEM000001',count:5},
+            {barcode:'ITEM000003',count:2.5},
+            {barcode:'ITEM000005',count:3},
+        ];
+
+        expect(actualResult).toEqual(expectResult);
+    });
+
+    it('should return tags_addCost2 it equals tags_addCount contain cost and discount', () => {
+        const tags_addCount=[
+          {barcode:'ITEM000001',count:5},
+          {barcode:'ITEM000003',count:2.5},
+          {barcode:'ITEM000005',count:3},
+        ];
+        const allItems=loadAllItems();
+        const promotions=loadPromotions();
+
+        const actualResult=getTags_addCost2(tags_addCount,allItems,promotions);
+
+        let expectResult = [
+            {barcode:'ITEM000001',count:5,cost:'15.00',discount_cost:'12.00'},
+            {barcode:'ITEM000003',count:2.5,cost:'37.50',discount_cost:'37.50'},
+            {barcode:'ITEM000005',count:3,cost:'13.50',discount_cost:'9.00'},
+        ];
+
+        expect(actualResult).toEqual(expectResult);
+    });
+
+  it('should return sum_discount_cost', () => {
+    const tags_addCost2=[
+      {barcode:'ITEM000001',count:5,cost:'15.00',discount_cost:'12.00'},
+      {barcode:'ITEM000003',count:2.5,cost:'37.50',discount_cost:'37.50'},
+      {barcode:'ITEM000005',count:3,cost:'13.50',discount_cost:'9.00'},
+    ];
+
+    const actualResult=getSum_discount_cost(tags_addCost2);
+
+    let expectResult = '58.50';
+
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it('should return savingMoney', () => {
+    const tags_addCost2=[
+      {barcode:'ITEM000001',count:5,cost:'15.00',discount_cost:'12.00'},
+      {barcode:'ITEM000003',count:2.5,cost:'37.50',discount_cost:'37.50'},
+      {barcode:'ITEM000005',count:3,cost:'13.50',discount_cost:'9.00'},
+    ];
+
+    const actualResult=getSavingMoney(tags_addCost2);
+
+    let expectResult = '7.50';
+
+    expect(actualResult).toEqual(expectResult);
+  });
+
+   it('should print text', () => {
 
     const tags = [
       'ITEM000001',
